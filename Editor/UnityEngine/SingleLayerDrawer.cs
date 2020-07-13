09ffc55086c0987fd7b4d8476b2059a13cd59d09
Laicasaane <laicasaane@outlook.com>
2020-07-13 16:38:55 +0700

    Add SingleLayer

---
+++ Editor/UnityEngine/SingleLayerDrawer.cs
@@ -0,0 +1,87 @@
+﻿/******************************************************************************
+ * Copyright (C) Leap Motion, Inc. 2011-2017.                                 *
+ * Leap Motion proprietary and  confidential.                                 *
+ *                                                                            *
+ * Use subject to the terms of the Leap Motion SDK Agreement available at     *
+ * https://developer.leapmotion.com/sdk_agreement, or another agreement       *
+ * between Leap Motion and you, your company or other organization.           *
+ ******************************************************************************/
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UnityEditor
+{
+    [CustomPropertyDrawer(typeof(SingleLayer))]
+    public class SingleLayerDrawer : PropertyDrawer
+    {
+        private GUIContent[] layerNames;
+        private List<int> layerValues;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EnsureLayersInitialized();
+
+            var valueProperty = property.FindPropertyRelative(nameof(SingleLayer.value));
+
+            if (valueProperty == null)
+            {
+                Debug.LogWarning("Could not find the layer index property, was it renamed or removed?");
+                return;
+            }
+
+            var index = this.layerValues.IndexOf(valueProperty.intValue);
+
+            if (index < 0)
+            {
+                if (Application.isPlaying)
+                {
+                    // If application is playing we dont want to change the layers on the fly
+                    // Instead, just display them as an int value
+                    valueProperty.intValue = EditorGUI.IntField(position, property.displayName, valueProperty.intValue);
+                    return;
+                }
+                else
+                {
+                    // If the application is not running, reset the layer to the default layer
+                    valueProperty.intValue = 0;
+                    index = 0;
+                }
+            }
+
+            var tooltipAttribute = this.fieldInfo.GetCustomAttributes(typeof(TooltipAttribute), true)
+                                                 .Cast<TooltipAttribute>()
+                                                 .FirstOrDefault();
+
+            if (tooltipAttribute != null)
+            {
+                label.tooltip = tooltipAttribute.tooltip;
+            }
+
+            index = EditorGUI.Popup(position, label, index, this.layerNames);
+            valueProperty.intValue = this.layerValues[index];
+        }
+
+        private void EnsureLayersInitialized()
+        {
+            if (this.layerNames == null)
+            {
+                var valueToLayer = new Dictionary<int, GUIContent>();
+
+                for (var i = 0; i < 32; i++)
+                {
+                    var layerName = LayerMask.LayerToName(i);
+
+                    if (!string.IsNullOrEmpty(layerName))
+                    {
+                        valueToLayer[i] = new GUIContent(layerName);
+                    }
+                }
+
+                this.layerValues = valueToLayer.Keys.ToList();
+                this.layerNames = valueToLayer.Values.ToArray();
+            }
+        }
+    }
+}
---
+++ UnityEngine/SingleLayer.cs
@@ -0,0 +1,84 @@
+﻿/******************************************************************************
+ * Copyright (C) Leap Motion, Inc. 2011-2017.                                 *
+ * Leap Motion proprietary and  confidential.                                 *
+ *                                                                            *
+ * Use subject to the terms of the Leap Motion SDK Agreement available at     *
+ * https://developer.leapmotion.com/sdk_agreement, or another agreement       *
+ * between Leap Motion and you, your company or other organization.           *
+ ******************************************************************************/
+
+using System;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// An object you can use to represent a single Unity layer
+    /// as a dropdown in the inspector.  Can be converted back and
+    /// forth between the integer representation Unity usually
+    /// uses in its own methods.
+    /// </summary>
+    [Serializable]
+    public struct SingleLayer : IEquatable<SingleLayer>
+    {
+        public int value;
+
+        public int mask
+        {
+            get
+            {
+                return 1 << this.value;
+            }
+
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("Single layer can only represent exactly one layer.  The provided mask represents no layers (mask was zero).");
+                }
+
+                var newIndex = 0;
+
+                while ((value & 1) == 0)
+                {
+                    value >>= 1;
+                    newIndex++;
+                }
+
+                if (value != 1)
+                {
+                    throw new ArgumentException("Single layer can only represent exactly one layer.  The provided mask represents more than one layer.");
+                }
+
+                this.value = newIndex;
+            }
+        }
+
+        public SingleLayer(int value)
+        {
+            this.value = value;
+        }
+
+        public SingleLayer(string name)
+        {
+            this.value = LayerMask.NameToLayer(name);
+        }
+
+        public override int GetHashCode()
+            => this.value.GetHashCode();
+
+        public override bool Equals(object obj)
+            => obj is SingleLayer other && this.value == other.value;
+
+        public bool Equals(SingleLayer other)
+            => this.value == other.value;
+
+        public override string ToString()
+            => LayerMask.LayerToName(this.value);
+
+        public static implicit operator int(SingleLayer value)
+            => value.value;
+
+        public static implicit operator SingleLayer(int value)
+            => new SingleLayer { value = value };
+    }
+}
