c9a711f08149a2537dc35cd1035d6ce59e6bdcf6
Laicasaane <laicasaane@outlook.com>
2020-07-13 16:46:02 +0700

    Add NumberExtensions

---
+++ Global/Extensions/NumberExtensions.cs
@@ -0,0 +1,32 @@
+﻿public static class NumberExtensions
+{
+    public static double Percent(this double value)
+        => value * 0.01;
+
+    public static double Percent(this long value)
+        => value * 0.01f;
+
+    public static double Percent(this ulong value)
+        => value * 0.01;
+
+    public static float Percent(this float value)
+        => value * 0.01f;
+
+    public static float Percent(this int value)
+        => value * 0.01f;
+
+    public static float Percent(this uint value)
+        => value * 0.01f;
+
+    public static float Percent(this short value)
+        => value * 0.01f;
+
+    public static float Percent(this ushort value)
+        => value * 0.01f;
+
+    public static float Percent(this byte value)
+        => value * 0.01f;
+
+    public static float Percent(this sbyte value)
+        => value * 0.01f;
+}
