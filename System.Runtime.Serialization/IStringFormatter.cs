48117193d8fe4c916917a36450a4e3b3293c924e
Laicasaane <laicasaane@outlook.com>
2020-07-14 18:39:07 +0700

    Add System.Runtime.Serialization.IStringFormatter interface

---
+++ System.Runtime.Serialization/IStringFormatter.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace System.Runtime.Serialization
+{
+    public interface IStringFormatter
+    {
+        string Serialize<T>(TextWriter writer, T @object);
+
+        T Deserialize<T>(TextReader reader);
+    }
+}
---
+++ System.Runtime.Serialization/IStringFormatter{T}.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace System.Runtime.Serialization
+{
+    public interface IStringFormatter<T>
+    {
+        string Serialize(TextWriter writer, T @object);
+
+        T Deserialize(TextReader reader);
+    }
+}
