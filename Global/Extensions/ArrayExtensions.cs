62a4b3ebccdf8df6b5725c3f4930bdbfc00c5693
Laicasaane <laicasaane@outlook.com>
2020-07-16 17:43:52 +0700

    Add Get methods

--- Global/Extensions/ArrayExtensions.cs
+++ Global/Extensions/ArrayExtensions.cs
@@ -7,4 +7,29 @@ public static class ArrayExtensions
 
     public static ReadArray<T> AsReadArray<T>(this T[] self)
         => self;
+
+    public static T Get<T>(this object[] self, int index)
+        => self.Length > index && self[index] is T val ? val : default;
+
+    public static object[] Get<T>(this object[] self, int index, out T value)
+    {
+        value = default;
+
+        if (self.Length > index && self[index] is T val)
+            value = val;
+
+        return self;
+    }
+
+    public static object[] GetThenMoveNext<T>(this object[] self, ref int index, out T value)
+    {
+        value = default;
+
+        if (self.Length > index && self[index] is T val)
+            value = val;
+
+        index += 1;
+
+        return self;
+    }
 }
