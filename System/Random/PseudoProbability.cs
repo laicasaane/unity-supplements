3a29be619e5874ce803102ba9ccf809801c4d03a
Laicasaane <laicasaane@outlook.com>
2020-07-27 03:01:32 +0700

    Better naming

--- System/Random/PseudoProbability.cs
+++ System/Random/PseudoProbability.cs
@@ -27,34 +27,55 @@ namespace System
             }
         }
 
-        public bool DoIHaveASimpleLuck(float chance)
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="chance">In the range of [0.0, 1.0]</param>
+        /// <returns></returns>
+        public bool DoIHaveLuck(float chance)
         {
-            var r = this.random.Range(0f, 100f);
-            return r <= chance;
-        }
-
-        public bool DoIHaveASimpleLuck(int chance)
-        {
-            var r = this.random.Range(0, 100);
+            var r = this.random.Range(0f, 1f);
             return r <= chance;
         }
 
         /// <summary>
         ///
         /// </summary>
-        /// <param name="chance">In the range of [0, 1]</param>
+        /// <param name="chance">In the range of [1.0, 100.0]</param>
+        /// <returns></returns>
+        public bool DoIHaveLuckInHundred(float chance)
+        {
+            var r = this.random.Range(1f, 100f);
+            return r <= chance;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="chance">In the range of [1, 100]</param>
+        /// <returns></returns>
+        public bool DoIHaveLuckInHundred(int chance)
+        {
+            var r = this.random.Range(1, 100);
+            return r <= chance;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="chance">In the range of [0.0, 1.0]</param>
         /// <param name="n"></param>
         /// <param name="n_0">Number of failed attempts</param>
         /// <param name="n_1">n_0 if succeeded or n+1 if failed</param>
         /// <returns></returns>
-        public bool DoIHaveAPercentLuck(float chance, int n, int n_0, out int n_1)
+        public bool DoIHaveLuck(float chance, int n, int n_0, out int n_1)
         {
             var thousand = this.math.RoundToInt(this.math.Clamp(chance, 0f, 1f) * 1000);
 
             if (thousand <= 0)
                 thousand = 1;
 
-            return DoIHaveLuck(thousand, n, n_0, out n_1);
+            return DoIHaveLuck_Internal(thousand, n, n_0, out n_1);
         }
 
         /// <summary>
@@ -65,24 +86,24 @@ namespace System
         /// <param name="n_0">Number of failed attempts</param>
         /// <param name="n_1">n_0 if succeeded or n+1 if failed</param>
         /// <returns></returns>
-        public bool DoIHaveAHundredLuck(int chance, int n, int n_0, out int n_1)
+        public bool DoIHaveLuckInHundred(int chance, int n, int n_0, out int n_1)
         {
             var thousand = this.math.Clamp(chance, 1, 100) * 10;
-            return DoIHaveLuck(thousand, n, n_0, out n_1);
+            return DoIHaveLuck_Internal(thousand, n, n_0, out n_1);
         }
 
         /// <summary>
         ///
         /// </summary>
-        /// <param name="chance">In the range of [1, 100]</param>
+        /// <param name="chance">In the range of [1.0, 100.0]</param>
         /// <param name="n"></param>
         /// <param name="n_0">Number of failed attempts</param>
         /// <param name="n_1">n_0 if succeeded or n+1 if failed</param>
         /// <returns></returns>
-        public bool DoIHaveAHundredLuck(float chance, int n, int n_0, out int n_1)
+        public bool DoIHaveLuckInHundred(float chance, int n, int n_0, out int n_1)
         {
             var thousand = this.math.RoundToInt(this.math.Clamp(chance, 1f, 100f) * 10);
-            return DoIHaveLuck(thousand, n, n_0, out n_1);
+            return DoIHaveLuck_Internal(thousand, n, n_0, out n_1);
         }
 
         /// <summary>
@@ -93,27 +114,27 @@ namespace System
         /// <param name="n_0">Number of failed attempts</param>
         /// <param name="n_1">n_0 if succeeded or n+1 if failed</param>
         /// <returns></returns>
-        public bool DoIHaveAThousandLuck(int chance, int n, int n_0, out int n_1)
+        public bool DoIHaveLuckInThousand(int chance, int n, int n_0, out int n_1)
         {
             var thousand = this.math.Clamp(chance, 1, 1000);
-            return DoIHaveLuck(thousand, n, n_0, out n_1);
+            return DoIHaveLuck_Internal(thousand, n, n_0, out n_1);
         }
 
         /// <summary>
         ///
         /// </summary>
-        /// <param name="chance">In the range of [1, 1000]</param>
+        /// <param name="chance">In the range of [1.0, 1000.0]</param>
         /// <param name="n"></param>
         /// <param name="n_0">Number of failed attempts</param>
         /// <param name="n_1">n_0 if succeeded or n+1 if failed</param>
         /// <returns></returns>
-        public bool DoIHaveAThousandLuck(float chance, int n, int n_0, out int n_1)
+        public bool DoIHaveLuckInThousand(float chance, int n, int n_0, out int n_1)
         {
             var thousand = this.math.RoundToInt(this.math.Clamp(chance, 1f, 1000f));
-            return DoIHaveLuck(thousand, n, n_0, out n_1);
+            return DoIHaveLuck_Internal(thousand, n, n_0, out n_1);
         }
 
-        private bool DoIHaveLuck(int thousand, int n, int n_0, out int n_1)
+        private bool DoIHaveLuck_Internal(int thousand, int n, int n_0, out int n_1)
         {
             var c = this.cValues[thousand];
             var p = c * n;
