b631683b4559be5354eb0afb0da28794e4d37c72
Laicasaane <laicasaane@outlook.com>
2020-08-24 01:33:00 +0700

    Add IPool<T> and Pool<T>

---
+++ System.Collections.Generic/Pools/IPool{T}.cs
@@ -0,0 +1,20 @@
+﻿namespace System.Collections.Generic
+{
+    public interface IGetOnlyPool<T>
+    {
+        T Get();
+    }
+
+    public interface IReturnOnlyPool<T>
+    {
+        void Return(T item);
+
+        void Return(params T[] items);
+
+        void Return(IEnumerable<T> items);
+    }
+
+    public interface IPool<T> : IReturnOnlyPool<T>, IGetOnlyPool<T>
+    {
+    }
+}
