9124d819a2970b738fd23e407841c43fc1042ff5
Laicasaane <laicasaane@outlook.com>
2020-10-21 23:04:16 +0700

    Add ReadHashSet<T>

---
+++ System.Collections.Generic/ReadCollections/Extensions/ReadHashSetTExtensions.cs
@@ -0,0 +1,80 @@
+namespace System.Collections.Generic
+{
+    public static class ReadHashSetTExtensions
+    {
+        public static bool ValidateIndex<T>(in this ReadHashSet<T> self, int index)
+            => self != null && index >= 0 && index < self.Count;
+
+        public static void GetRange<T>(in this ReadHashSet<T> self, in ReadRange<int> range, ICollection<T> output, bool allowDuplicate = true, bool allowNull = false)
+        {
+            var start = Math.Min(range.Start, range.End);
+            var end = Math.Max(range.Start, range.End);
+
+            self.GetRange(start + 1, end - start, output, allowDuplicate, allowNull);
+        }
+
+        public static void GetRange<T>(in this ReadHashSet<T> self, int offset, ICollection<T> output, bool allowDuplicate = true, bool allowNull = false)
+            => self.GetRange(offset, -1, output, allowDuplicate, allowNull);
+
+        public static void GetRange<T>(in this ReadHashSet<T> self, int offset, int count, ICollection<T> output, bool allowDuplicate = true, bool allowNull = false)
+        {
+            if (output == null || count == 0)
+                return;
+
+            offset = Math.Max(offset, 0);
+
+            if (offset > self.Count)
+                throw new IndexOutOfRangeException(nameof(offset));
+
+            if (count < 0)
+                count = self.Count - offset;
+            else
+                count += offset;
+
+            if (count > self.Count)
+                throw new IndexOutOfRangeException(nameof(count));
+
+            var o = 0;
+            var c = 0;
+
+            if (allowDuplicate)
+            {
+                foreach (var item in self)
+                {
+                    if (o < offset)
+                    {
+                        o += 1;
+                        continue;
+                    }
+
+                    if (c >= count)
+                        break;
+
+                    if (allowNull || item != null)
+                        output.Add(item);
+
+                    c += 1;
+                }
+
+                return;
+            }
+
+            foreach (var item in self)
+            {
+                if (o < offset)
+                {
+                    o += 1;
+                    continue;
+                }
+
+                if (c >= count)
+                    break;
+
+                if ((allowNull || item != null) && !output.Contains(item))
+                    output.Add(item);
+
+                c += 1;
+            }
+        }
+    }
+}
---
+++ System.Collections.Generic/ReadCollections/ReadHashSet{T}.cs
@@ -0,0 +1,97 @@
+using System.Runtime.CompilerServices;
+
+namespace System.Collections.Generic
+{
+    public readonly struct ReadHashSet<T> : IReadOnlyCollection<T>, IEquatableReadOnlyStruct<ReadHashSet<T>>
+    {
+        private readonly HashSet<T> source;
+        private readonly bool hasSource;
+
+        public ReadHashSet(HashSet<T> source)
+        {
+            this.source = source ?? _empty;
+            this.Count = this.source.Count;
+            this.hasSource = true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal HashSet<T> GetSource()
+            => this.hasSource ? (this.source ?? _empty) : _empty;
+
+        public int Count { get; }
+
+        public override int GetHashCode()
+            => GetSource().GetHashCode();
+
+        public override bool Equals(object obj)
+            => obj is ReadHashSet<T> other && Equals(in other);
+
+        public bool Equals(ReadHashSet<T> other)
+        {
+            var source = GetSource();
+            var otherSource = other.GetSource();
+
+            return source == otherSource;
+        }
+
+        public bool Equals(in ReadHashSet<T> other)
+        {
+            var source = GetSource();
+            var otherSource = other.GetSource();
+
+            return source == otherSource;
+        }
+
+        public bool Contains(T item)
+            => GetSource().Contains(item);
+
+        public void CopyTo(T[] array, int arrayIndex, int count)
+            => GetSource().CopyTo(array, arrayIndex, count);
+
+        public void CopyTo(T[] array, int arrayIndex)
+            => GetSource().CopyTo(array, arrayIndex);
+
+        public void CopyTo(T[] array)
+            => GetSource().CopyTo(array);
+
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+            => GetSource().IsProperSubsetOf(other);
+
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+            => GetSource().IsProperSupersetOf(other);
+
+        public bool IsSubsetOf(IEnumerable<T> other)
+            => GetSource().IsSubsetOf(other);
+
+        public bool IsSupersetOf(IEnumerable<T> other)
+            => GetSource().IsSupersetOf(other);
+
+        public bool Overlaps(IEnumerable<T> other)
+            => GetSource().Overlaps(other);
+
+        public bool SetEquals(IEnumerable<T> other)
+            => GetSource().SetEquals(other);
+
+        public HashSet<T>.Enumerator GetEnumerator()
+            => GetSource().GetEnumerator();
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+            => GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        private static HashSet<T> _empty { get; } = new HashSet<T>();
+
+        public static ReadHashSet<T> Empty { get; } = new ReadHashSet<T>(_empty);
+
+        public static implicit operator ReadHashSet<T>(HashSet<T> source)
+            => source == null ? Empty : new ReadHashSet<T>(source);
+
+        public static bool operator ==(in ReadHashSet<T> a, in ReadHashSet<T> b)
+            => a.Equals(in b);
+
+        public static bool operator !=(in ReadHashSet<T> a, in ReadHashSet<T> b)
+            => !a.Equals(in b);
+    }
+}
