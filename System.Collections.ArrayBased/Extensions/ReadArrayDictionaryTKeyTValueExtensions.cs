c39a9564c5780f95628647d6aceff7f516854e81
Laicasaane <laicasaane@outlook.com>
2021-01-31 22:45:38 +0700

    Add extensions for readonly version of array-based collections

---
+++ System.Collections.ArrayBased/Extensions/ReadArrayDictionaryTKeyTValueExtensions.cs
@@ -0,0 +1,243 @@
+﻿using System.Collections.Generic;
+
+namespace System.Collections.ArrayBased
+{
+    public static class ReadArrayDictionaryTKeyTValueExtensions
+    {
+        public static bool ValidateIndex<TKey, TValue>(in this ReadArrayDictionary<TKey, TValue> self, int index)
+            => index >= 0 && index < self.Count;
+
+        public static bool ValidateIndex<TKey, TValue>(in this ReadArrayDictionary<TKey, TValue> self, uint index)
+            => index >= 0 && index < self.Count;
+
+        public static void GetRange<TKey, TValue>(in this ReadArrayDictionary<TKey, TValue> self, IEnumerable<TKey> keys, Dictionary<TKey, TValue> output, bool allowDuplicateValue = false, bool allowNull = false)
+        {
+            if (keys == null || output == null)
+                return;
+
+            if (allowDuplicateValue)
+            {
+                foreach (var key in keys)
+                {
+                    if (output.ContainsKey(key) ||
+                        !self.TryGetValue(key, out var value))
+                        continue;
+
+                    if (allowNull || value != null)
+                        output.Add(key, value);
+                }
+
+                return;
+            }
+
+            foreach (var key in keys)
+            {
+                if (output.ContainsKey(key) ||
+                    !self.TryGetValue(key, out var value))
+                    continue;
+
+                if ((allowNull || value != null) && !output.ContainsValue(value))
+                    output.Add(key, value);
+            }
+        }
+
+        public static void GetRange<TKey, TValue>(in this ReadArrayDictionary<TKey, TValue> self, IEnumerable<TKey> keys, ArrayDictionary<TKey, TValue> output, bool allowDuplicateValue = false, bool allowNull = false)
+        {
+            if (keys == null || output == null)
+                return;
+
+            if (allowDuplicateValue)
+            {
+                foreach (var key in keys)
+                {
+                    if (output.ContainsKey(key) ||
+                        !self.TryGetValue(key, out var value))
+                        continue;
+
+                    if (allowNull || value != null)
+                        output.Set(key, value);
+                }
+
+                return;
+            }
+
+            foreach (var key in keys)
+            {
+                if (output.ContainsKey(key) ||
+                    !self.TryGetValue(key, out var value))
+                    continue;
+
+                if ((allowNull || value != null) && !output.ContainsValue(value))
+                    output.Set(key, value);
+            }
+        }
+
+        public static void GetRangeIn<TKey, TValue>(in this ReadArrayDictionary<TKey, TValue> self, IEnumerable<TKey> keys, ArrayDictionary<TKey, TValue> output, bool allowDuplicateValue = false, bool allowNull = false)
+        {
+            if (keys == null || output == null)
+                return;
+
+            if (allowDuplicateValue)
+            {
+                foreach (var key in keys)
+                {
+                    if (output.ContainsKey(in key) ||
+                        !self.TryGetValue(key, out var value))
+                        continue;
+
+                    if (allowNull || value != null)
+                        output.Set(in key, in value);
+                }
+
+                return;
+            }
+
+            foreach (var key in keys)
+            {
+                if (output.ContainsKey(in key) ||
+                    !self.TryGetValue(key, out var value))
+                    continue;
+
+                if ((allowNull || value != null) && !output.ContainsValue(in value))
+                    output.Set(in key, in value);
+            }
+        }
+
+        public static void GetRangeInKey<TKey, TValue>(in this ReadArrayDictionary<TKey, TValue> self, IEnumerable<TKey> keys, ArrayDictionary<TKey, TValue> output, bool allowDuplicateValue = false, bool allowNull = false)
+        {
+            if (keys == null || output == null)
+                return;
+
+            if (allowDuplicateValue)
+            {
+                foreach (var key in keys)
+                {
+                    if (output.ContainsKey(in key) ||
+                        !self.TryGetValue(key, out var value))
+                        continue;
+
+                    if (allowNull || value != null)
+                        output.Set(in key, value);
+                }
+
+                return;
+            }
+
+            foreach (var key in keys)
+            {
+                if (output.ContainsKey(in key) ||
+                    !self.TryGetValue(key, out var value))
+                    continue;
+
+                if ((allowNull || value != null) && !output.ContainsValue(value))
+                    output.Set(in key, value);
+            }
+        }
+
+        public static void GetRangeInValue<TKey, TValue>(in this ReadArrayDictionary<TKey, TValue> self, IEnumerable<TKey> keys, ArrayDictionary<TKey, TValue> output, bool allowDuplicateValue = false, bool allowNull = false)
+        {
+            if (keys == null || output == null)
+                return;
+
+            if (allowDuplicateValue)
+            {
+                foreach (var key in keys)
+                {
+                    if (output.ContainsKey(key) ||
+                        !self.TryGetValue(key, out var value))
+                        continue;
+
+                    if (allowNull || value != null)
+                        output.Set(key, in value);
+                }
+
+                return;
+            }
+
+            foreach (var key in keys)
+            {
+                if (output.ContainsKey(key) ||
+                    !self.TryGetValue(key, out var value))
+                    continue;
+
+                if ((allowNull || value != null) && !output.ContainsValue(in value))
+                    output.Set(key, in value);
+            }
+        }
+
+        public static void GetRange<TKey, TValue>(in this ReadArrayDictionary<TKey, TValue> self, IEnumerable<TKey> keys, IDictionary<TKey, TValue> output, bool allowNull = false)
+        {
+            if (keys == null || output == null)
+                return;
+
+            foreach (var key in keys)
+            {
+                if (output.ContainsKey(key) ||
+                    !self.TryGetValue(key, out var value))
+                    continue;
+
+                if (allowNull || value != null)
+                    output.Add(key, value);
+            }
+        }
+
+        public static void GetRange<TKey, TValue>(in this ReadArrayDictionary<TKey, TValue> self, IEnumerable<TKey> keys, ICollection<TValue> output, bool allowDuplicate = true, bool allowNull = false)
+        {
+            if (keys == null || output == null)
+                return;
+
+            if (allowDuplicate)
+            {
+                foreach (var key in keys)
+                {
+                    if (!self.TryGetValue(key, out var value))
+                        continue;
+
+                    if (allowNull || value != null)
+                        output.Add(value);
+                }
+
+                return;
+            }
+
+            foreach (var key in keys)
+            {
+                if (!self.TryGetValue(key, out var value))
+                    continue;
+
+                if ((allowNull || value != null) && !output.Contains(value))
+                    output.Add(value);
+            }
+        }
+
+        public static void GetRange<TKey, TValue>(in this ReadArrayDictionary<TKey, TValue> self, IEnumerable<TKey> keys, ICollection<KeyValuePair<TKey, TValue>> output, bool allowDuplicate = true, bool allowNull = false)
+        {
+            if (keys == null || output == null)
+                return;
+
+            if (allowDuplicate)
+            {
+                foreach (var key in keys)
+                {
+                    if (!self.TryGetValue(key, out var value))
+                        continue;
+
+                    if (allowNull || value != null)
+                        output.Add(new KeyValuePair<TKey, TValue>(key, value));
+                }
+
+                return;
+            }
+
+            foreach (var key in keys)
+            {
+                if (!self.TryGetValue(key, out var value))
+                    continue;
+
+                var kvp = new KeyValuePair<TKey, TValue>(key, value);
+                if ((allowNull || value != null) && !output.Contains(kvp))
+                    output.Add(kvp);
+            }
+        }
+    }
+}
---
+++ System.Collections.ArrayBased/Extensions/ReadArrayListTExtensions.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+
+namespace System.Collections.ArrayBased
+{
+    public static class ReadArrayListTExtensions
+    {
+        public static bool ValidateIndex<T>(in this ReadArrayList<T> self, int index)
+            => index >= 0 && index < self.Count;
+
+        public static bool ValidateIndex<T>(in this ReadArrayList<T> self, uint index)
+            => index >= 0 && index < self.Count;
+
+        public static void GetRange<T>(in this ReadArrayList<T> self, in ReadRange<uint> range, ArrayList<T> output, bool allowDuplicate = true, bool allowNull = false)
+        {
+            var start = Math.Min(range.Start, range.End);
+            var end = Math.Max(range.Start, range.End);
+
+            self.GetRange(start, end - start + 1, output, allowDuplicate, allowNull);
+        }
+
+        public static void GetRange<T>(in this ReadArrayList<T> self, long offset, ArrayList<T> output, bool allowDuplicate = true, bool allowNull = false)
+            => self.GetRange(offset, -1, output, allowDuplicate, allowNull);
+
+        public static void GetRange<T>(in this ReadArrayList<T> self, long offset, long count, ArrayList<T> output, bool allowDuplicate = true, bool allowNull = false)
+        {
+            if (output == null || count == 0)
+                return;
+
+            Validate(self.Count, ref offset, ref count);
+
+            if (allowDuplicate)
+            {
+                for (var i = (uint)offset; i < count; i++)
+                {
+                    if (allowNull || self[i] != null)
+                        output.Add(self[i]);
+                }
+
+                return;
+            }
+
+            for (var i = (uint)offset; i < count; i++)
+            {
+                if ((allowNull || self[i] != null) && !output.Contains(self[i]))
+                    output.Add(self[i]);
+            }
+        }
+
+        public static void GetRangeIn<T>(in this ReadArrayList<T> self, in ReadRange<uint> range, ArrayList<T> output, bool allowDuplicate = true, bool allowNull = false)
+        {
+            var start = Math.Min(range.Start, range.End);
+            var end = Math.Max(range.Start, range.End);
+
+            self.GetRangeIn(start, end - start + 1, output, allowDuplicate, allowNull);
+        }
+
+        public static void GetRangeIn<T>(in this ReadArrayList<T> self, long offset, ArrayList<T> output, bool allowDuplicate = true, bool allowNull = false)
+            => self.GetRangeIn(offset, -1, output, allowDuplicate, allowNull);
+
+        public static void GetRangeIn<T>(in this ReadArrayList<T> self, long offset, long count, ArrayList<T> output, bool allowDuplicate = true, bool allowNull = false)
+        {
+            if (output == null || count == 0)
+                return;
+
+            Validate(self.Count, ref offset, ref count);
+
+            if (allowDuplicate)
+            {
+                for (var i = (uint)offset; i < count; i++)
+                {
+                    if (allowNull || self[i] != null)
+                        output.Add(in self[i]);
+                }
+
+                return;
+            }
+
+            for (var i = (uint)offset; i < count; i++)
+            {
+                if ((allowNull || self[i] != null) && !output.Contains(in self[i]))
+                    output.Add(in self[i]);
+            }
+        }
+
+        public static void GetRange<T>(in this ReadArrayList<T> self, in ReadRange<uint> range, ICollection<T> output, bool allowDuplicate = true, bool allowNull = false)
+        {
+            var start = Math.Min(range.Start, range.End);
+            var end = Math.Max(range.Start, range.End);
+
+            self.GetRange(start, end - start + 1, output, allowDuplicate, allowNull);
+        }
+
+        public static void GetRange<T>(in this ReadArrayList<T> self, long offset, ICollection<T> output, bool allowDuplicate = true, bool allowNull = false)
+            => self.GetRange(offset, -1, output, allowDuplicate, allowNull);
+
+        public static void GetRange<T>(in this ReadArrayList<T> self, long offset, long count, ICollection<T> output, bool allowDuplicate = true, bool allowNull = false)
+        {
+            if (output == null || count == 0)
+                return;
+
+            Validate(self.Count, ref offset, ref count);
+
+            if (allowDuplicate)
+            {
+                for (var i = (uint)offset; i < count; i++)
+                {
+                    if (allowNull || self[i] != null)
+                        output.Add(self[i]);
+                }
+
+                return;
+            }
+
+            for (var i = (uint)offset; i < count; i++)
+            {
+                if ((allowNull || self[i] != null) && !output.Contains(self[i]))
+                    output.Add(self[i]);
+            }
+        }
+
+        private static void Validate(uint listCount, ref long offset, ref long count)
+        {
+            offset = Math.Max(offset, 0);
+
+            if (offset > listCount)
+                throw new IndexOutOfRangeException(nameof(offset));
+
+            if (count < 0)
+                count = listCount - offset;
+            else
+                count += offset;
+
+            if (count > listCount)
+                throw new IndexOutOfRangeException(nameof(count));
+        }
+    }
+}
