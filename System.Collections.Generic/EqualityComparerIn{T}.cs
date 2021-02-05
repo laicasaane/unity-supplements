969597b9c703b7130e3062d4fafd9d098a2ed2c6
Laicasaane <laicasaane@outlook.com>
2021-02-05 18:10:06 +0700

    Add EqualityComparerIn<T>

---
+++ System.Collections.Generic/EqualityComparerIn{T}.cs
@@ -0,0 +1,19 @@
+﻿namespace System.Collections.Generic
+{
+    public class EqualityComparerIn<T> : IEqualityComparerIn<T>
+    {
+        public bool Equals(in T x, in T y)
+            => x.Equals(y);
+
+        public bool Equals(T x, T y)
+            => x.Equals(y);
+
+        public int GetHashCode(in T obj)
+            => obj.GetHashCode();
+
+        public int GetHashCode(T obj)
+            => obj.GetHashCode();
+
+        public static EqualityComparerIn<T> Default { get; } = new EqualityComparerIn<T>();
+    }
+}
