ff2ef0941d5c250cef766c250dafe6890d9f37cc
Laicasaane <laicasaane@outlook.com>
2021-02-22 23:39:29 +0700

    Add SGridIndexRange

---
+++ System.Grid/SGridIndexRange.Enumerator.cs
@@ -0,0 +1,279 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace System.Grid
+{
+    public partial struct SGridIndexRange
+    {
+
+        public static implicit operator ReadRange<SGridIndex, Enumerator.ColumnFirst>(in SGridIndexRange value)
+            => new ReadRange<SGridIndex, Enumerator.ColumnFirst>(value.Start, value.End, value.IsFromEnd);
+
+        public static implicit operator ReadRange<SGridIndex, Enumerator.RowFirst>(in SGridIndexRange value)
+            => new ReadRange<SGridIndex, Enumerator.RowFirst>(value.Start, value.End, value.IsFromEnd);
+
+        public static implicit operator ReadRange<SGridIndex>(in SGridIndexRange value)
+        {
+            if (value.Direction == GridDirection.Row)
+                return new ReadRange<SGridIndex>(value.Start, value.End, value.IsFromEnd, new Enumerator.RowFirst());
+            else
+                return new ReadRange<SGridIndex>(value.Start, value.End, value.IsFromEnd, new Enumerator.ColumnFirst());
+        }
+
+        public static implicit operator SGridIndexRange(in ReadRange<SGridIndex, Enumerator.ColumnFirst> value)
+            => new SGridIndexRange(value.Start, value.End, value.IsFromEnd, GridDirection.Column);
+
+        public static implicit operator SGridIndexRange(in ReadRange<SGridIndex, Enumerator.RowFirst> value)
+            => new SGridIndexRange(value.Start, value.End, value.IsFromEnd, GridDirection.Row);
+
+        public struct Enumerator : IEnumerator<SGridIndex>
+        {
+            private readonly SGridIndex start, end;
+            private readonly int startValue, endValue;
+            private readonly sbyte rowSign, colSign, compare;
+            private readonly bool byRow;
+
+            private SGridIndex current;
+            private sbyte flag;
+
+            public Enumerator(in SGridIndexRange range)
+                : this(range.Start, range.End, range.IsFromEnd, range.Direction)
+            { }
+
+            public Enumerator(in SGridIndex start, in SGridIndex end, bool fromEnd, GridDirection direction)
+            {
+                this.byRow = direction == GridDirection.Row;
+                this.colSign = default;
+
+                var minCol = Math.Min(start.Column, end.Column);
+                var maxCol = Math.Max(start.Column, end.Column);
+                var colCount = Math.Abs(maxCol - minCol) + 1;
+
+                if (fromEnd)
+                {
+                    this.start = end;
+                    this.end = start;
+                }
+                else
+                {
+                    this.start = start;
+                    this.end = end;
+                }
+
+                var startIndex = start.ToIndex1(colCount);
+                var endIndex = end.ToIndex1(colCount);
+                var increasing = startIndex <= endIndex;
+
+                if (increasing)
+                {
+                    if (fromEnd)
+                    {
+                        if (this.byRow)
+                        {
+                            this.rowSign = -1;
+                            this.colSign = (sbyte)(this.start.Column.CompareTo(this.end.Column) * -1);
+                            this.startValue = Math.Max(this.start.Row, this.end.Row);
+                            this.endValue = Math.Min(this.start.Row, this.end.Row);
+
+                            if (this.start.Column.CompareTo(this.end.Column) < 0)
+                            {
+                                this.startValue -= 1;
+                                this.compare = 0;
+                            }
+                            else
+                            {
+                                this.compare = -1;
+                            }
+                        }
+                        else
+                        {
+                            this.rowSign = this.colSign = this.compare = -1;
+                            this.startValue = Math.Max(this.start.Column, this.end.Column);
+                            this.endValue = Math.Min(this.start.Column, this.end.Column);
+                        }
+                    }
+                    else
+                    {
+                        if (this.byRow)
+                        {
+                            this.rowSign = 1;
+                            this.colSign = (sbyte)(this.start.Column.CompareTo(this.end.Column) * -1);
+
+                            this.startValue = Math.Min(this.start.Row, this.end.Row);
+                            this.endValue = Math.Max(this.start.Row, this.end.Row);
+
+                            if (this.start.Column.CompareTo(this.end.Column) > 0)
+                            {
+                                this.startValue += 1;
+                                this.compare = 0;
+                            }
+                            else
+                            {
+                                this.compare = 1;
+                            }
+                        }
+                        else
+                        {
+                            this.rowSign = this.colSign = this.compare = 1;
+                            this.startValue = Math.Min(this.start.Column, this.end.Column);
+                            this.endValue = Math.Max(this.start.Column, this.end.Column);
+                        }
+                    }
+                }
+                else
+                {
+                    if (fromEnd)
+                    {
+                        if (this.byRow)
+                        {
+                            this.rowSign = 1;
+                            this.colSign = (sbyte)(this.start.Column.CompareTo(this.end.Column) * -1);
+                            this.startValue = Math.Min(this.start.Row, this.end.Row);
+                            this.endValue = Math.Max(this.start.Row, this.end.Row);
+
+                            if (this.start.Column.CompareTo(this.end.Column) > 0)
+                            {
+                                this.startValue += 1;
+                                this.compare = 0;
+                            }
+                            else
+                            {
+                                this.compare = 1;
+                            }
+                        }
+                        else
+                        {
+                            this.rowSign = this.colSign = this.compare = 1;
+                            this.startValue = Math.Min(this.start.Column, this.end.Column);
+                            this.endValue = Math.Max(this.start.Column, this.end.Column);
+                        }
+                    }
+                    else
+                    {
+                        if (this.byRow)
+                        {
+                            this.rowSign = -1;
+                            this.colSign = (sbyte)(this.start.Column.CompareTo(this.end.Column) * -1);
+                            this.startValue = Math.Max(this.start.Row, this.end.Row);
+                            this.endValue = Math.Min(this.start.Row, this.end.Row);
+
+                            if (this.start.Column.CompareTo(this.end.Column) < 0)
+                            {
+                                this.startValue -= 1;
+                                this.compare = 0;
+                            }
+                            else
+                            {
+                                this.compare = -1;
+                            }
+                        }
+                        else
+                        {
+                            this.rowSign = this.colSign = this.compare = -1;
+                            this.startValue = Math.Max(this.start.Column, this.end.Column);
+                            this.endValue = Math.Min(this.start.Column, this.end.Column);
+                        }
+                    }
+                }
+
+                this.current = this.start;
+                this.flag = (sbyte)(this.current == this.end ? 1 : -1);
+            }
+
+            public Enumerator(GridDirection direction)
+            {
+                this.byRow = direction == GridDirection.Row;
+                this.current = this.start = this.end = default;
+                this.startValue = this.endValue = default;
+                this.rowSign = this.colSign = this.compare = this.flag = default;
+            }
+
+            public bool MoveNext()
+            {
+                if (this.flag == 0)
+                {
+                    if (this.current == this.end)
+                    {
+                        this.flag = 1;
+                        return false;
+                    }
+
+                    int row, col;
+
+                    if (this.byRow)
+                    {
+                        row = this.current.Row + this.rowSign;
+                        col = this.current.Column;
+
+                        if (row.CompareTo(this.endValue) == this.compare && col != this.end.Column)
+                        {
+                            col += this.colSign;
+                            row = this.startValue;
+                        }
+                    }
+                    else
+                    {
+                        col = this.current.Column + this.colSign;
+                        row = this.current.Row;
+
+                        if (col.CompareTo(this.endValue) == this.compare)
+                        {
+                            row += this.rowSign;
+                            col = this.startValue;
+                        }
+                    }
+
+                    this.current = new SGridIndex(row, col);
+                    return true;
+                }
+
+                if (this.flag < 0)
+                {
+                    this.flag = 0;
+                    return true;
+                }
+
+                return false;
+            }
+
+            public SGridIndex Current
+            {
+                get
+                {
+                    if (this.flag < 0)
+                        throw ThrowHelper.GetInvalidOperationException_InvalidOperation_EnumNotStarted();
+
+                    if (this.flag > 0)
+                        throw ThrowHelper.GetInvalidOperationException_InvalidOperation_EnumEnded();
+
+                    return this.current;
+                }
+            }
+
+            object IEnumerator.Current
+                => this.Current;
+
+            public void Reset()
+            {
+                this.current = this.start;
+                this.flag = -1;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public readonly struct ColumnFirst : IRangeEnumerator<SGridIndex>
+            {
+                public IEnumerator<SGridIndex> Enumerate(SGridIndex start, SGridIndex end, bool fromEnd)
+                    => new Enumerator(start, end, fromEnd, GridDirection.Column);
+            }
+
+            public readonly struct RowFirst : IRangeEnumerator<SGridIndex>
+            {
+                public IEnumerator<SGridIndex> Enumerate(SGridIndex start, SGridIndex end, bool fromEnd)
+                    => new Enumerator(start, end, fromEnd, GridDirection.Row);
+            }
+        }
+    }
+}
---
+++ System.Grid/SGridIndexRange.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace System.Grid
+{
+    [Serializable]
+    public readonly partial struct SGridIndexRange : IRange<SGridIndex, SGridIndexRange.Enumerator>,
+                                                    IEquatableReadOnlyStruct<SGridIndexRange>, ISerializable
+    {
+        public SGridIndex Start { get; }
+
+        public SGridIndex End { get; }
+
+        public bool IsFromEnd { get; }
+
+        public GridDirection Direction { get; }
+
+        public SGridIndexRange(in SGridIndex start, in SGridIndex end, bool fromEnd = false, GridDirection direction = default)
+        {
+            this.Start = start;
+            this.End = end;
+            this.IsFromEnd = fromEnd;
+            this.Direction = direction;
+        }
+
+        private SGridIndexRange(SerializationInfo info, StreamingContext context)
+        {
+            this.Start = info.GetValueOrDefault<SGridIndex>(nameof(this.Start));
+            this.End = info.GetValueOrDefault<SGridIndex>(nameof(this.End));
+            this.IsFromEnd = info.GetBooleanOrDefault(nameof(this.IsFromEnd));
+            this.Direction = info.GetValueOrDefault<GridDirection>(nameof(this.Direction));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(nameof(this.Start), this.Start);
+            info.AddValue(nameof(this.End), this.End);
+            info.AddValue(nameof(this.IsFromEnd), this.IsFromEnd);
+            info.AddValue(nameof(this.Direction), this.Direction);
+        }
+
+        public void Deconstruct(out SGridIndex start, out SGridIndex end, out bool fromEnd, out GridDirection direction)
+        {
+            start = this.Start;
+            end = this.End;
+            fromEnd = this.IsFromEnd;
+            direction = this.Direction;
+        }
+
+        public SGridIndexRange With(in SGridIndex? Start = null, in SGridIndex? End = null, bool? IsFromEnd = null, GridDirection? Direction = null)
+            => new SGridIndexRange(
+                Start ?? this.Start,
+                End ?? this.End,
+                IsFromEnd ?? this.IsFromEnd,
+                Direction ?? this.Direction
+            );
+
+        public SGridIndexRange ByRow()
+            => new SGridIndexRange(this.Start, this.End, this.IsFromEnd, GridDirection.Row);
+
+        public SGridIndexRange ByColumn()
+            => new SGridIndexRange(this.Start, this.End, this.IsFromEnd, GridDirection.Column);
+
+        public SGridIndexRange FromStart()
+            => new SGridIndexRange(this.Start, this.End, false, this.Direction);
+
+        public SGridIndexRange FromEnd()
+            => new SGridIndexRange(this.Start, this.End, true, this.Direction);
+
+        IRange<SGridIndex> IRange<SGridIndex>.FromStart()
+            => FromStart();
+
+        IRange<SGridIndex> IRange<SGridIndex>.FromEnd()
+            => FromEnd();
+
+        public bool Contains(in SGridIndex value)
+        {
+            var containsRow = this.Start.Row.CompareTo(this.End.Row) <= 0
+                              ? value.Row >= this.Start.Row && value.Row <= this.End.Row
+                              : value.Row >= this.End.Row && value.Row <= this.Start.Row;
+
+            var containsCol = this.Start.Column.CompareTo(this.End.Column) <= 0
+                              ? value.Column >= this.Start.Column && value.Column <= this.End.Column
+                              : value.Column >= this.End.Column && value.Column <= this.Start.Column;
+
+            return containsRow && containsCol;
+        }
+
+        public int Count()
+        {
+            var row = Math.Max(this.End.Row - this.Start.Row + 1, 0);
+            var col = Math.Max(this.End.Column - this.Start.Column + 1, 0);
+            return row * col;
+        }
+
+        public override bool Equals(object obj)
+            => obj is SGridIndexRange other &&
+               this.Start == other.Start &&
+               this.End == other.End &&
+               this.IsFromEnd == other.IsFromEnd &&
+               this.Direction == other.Direction;
+
+        public bool Equals(in SGridIndexRange other)
+            => this.Start == other.Start &&
+               this.End == other.End &&
+               this.IsFromEnd == other.IsFromEnd &&
+               this.Direction == other.Direction;
+
+        public bool Equals(SGridIndexRange other)
+            => this.Start == other.Start &&
+               this.End == other.End &&
+               this.IsFromEnd == other.IsFromEnd &&
+               this.Direction == other.Direction;
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1418356749;
+            hashCode = hashCode * -1521134295 + this.Start.GetHashCode();
+            hashCode = hashCode * -1521134295 + this.End.GetHashCode();
+            hashCode = hashCode * -1521134295 + this.IsFromEnd.GetHashCode();
+            hashCode = hashCode * -1521134295 + this.Direction.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+            => $"{{ {nameof(this.Start)}={this.Start}, {nameof(this.End)}={this.End}, {nameof(this.IsFromEnd)}={this.IsFromEnd}, {nameof(this.Direction)}={this.Direction} }}";
+
+        public Enumerator GetEnumerator()
+            => new Enumerator(this);
+
+        public Enumerator Range()
+            => GetEnumerator();
+
+        IEnumerator<SGridIndex> IRange<SGridIndex>.Range()
+            => GetEnumerator();
+
+        public SGridIndexRange Normalize()
+            => Normal(this.Start, this.End, this.IsFromEnd, this.Direction);
+
+        public bool Intersects(in SGridIndexRange other)
+        {
+            var nThis = Normalize();
+            var nOther = other.Normalize();
+
+            return (nOther.Start.Row <= nThis.End.Row && nOther.Start.Column <= nThis.End.Column &&
+                    nOther.End.Row >= nThis.Start.Row && nOther.End.Column >= nThis.Start.Column);
+        }
+
+        /// <summary>
+        /// Create a normal range from (a, b) where <see cref="Start"/> is lesser than <see cref="End"/>.
+        /// </summary>
+        public static SGridIndexRange Normal(in SGridIndex a, in SGridIndex b, bool fromEnd = false, GridDirection direction = default)
+        {
+            var rowIncreasing = a.Row.CompareTo(b.Row) <= 0;
+            var colIncreasing = a.Column.CompareTo(b.Column) <= 0;
+
+            var start = new SGridIndex(
+                rowIncreasing ? a.Row : b.Row,
+                colIncreasing ? a.Column : b.Column
+            );
+
+            var end = new SGridIndex(
+                rowIncreasing ? b.Row : a.Row,
+                colIncreasing ? b.Column : a.Column
+            );
+
+            return new SGridIndexRange(start, end, fromEnd, direction);
+        }
+
+        public static SGridIndexRange FromSize(in SGridIndex value, bool fromEnd = false)
+            => new SGridIndexRange(SGridIndex.Zero, value - SGridIndex.One, fromEnd);
+
+        public static SGridIndexRange FromStart(in SGridIndex start, in SGridIndex end)
+            => new SGridIndexRange(start, end, false);
+
+        public static SGridIndexRange FromEnd(in SGridIndex start, in SGridIndex end)
+            => new SGridIndexRange(start, end, true);
+
+        public static implicit operator SGridIndexRange(in (SGridIndex start, SGridIndex end) value)
+            => new SGridIndexRange(value.start, value.end);
+
+        public static implicit operator SGridIndexRange(in (SGridIndex start, SGridIndex end, bool fromEnd) value)
+            => new SGridIndexRange(value.start, value.end, value.fromEnd);
+
+        public static implicit operator SGridIndexRange(in (SGridIndex start, SGridIndex end, bool fromEnd, GridDirection direction) value)
+            => new SGridIndexRange(value.start, value.end, value.fromEnd, value.direction);
+
+        public static implicit operator SGridIndexRange(in ReadRange<SGridIndex> value)
+            => new SGridIndexRange(value.Start, value.End, value.IsFromEnd);
+
+        public static bool operator ==(in SGridIndexRange lhs, in SGridIndexRange rhs)
+            => lhs.Start == rhs.Start &&
+               lhs.End == rhs.End &&
+               lhs.IsFromEnd == rhs.IsFromEnd &&
+               lhs.Direction == rhs.Direction;
+
+        public static bool operator !=(in SGridIndexRange lhs, in SGridIndexRange rhs)
+            => lhs.Start != rhs.Start ||
+               lhs.End != rhs.End ||
+               lhs.IsFromEnd != rhs.IsFromEnd ||
+               lhs.Direction != rhs.Direction;
+    }
+}
