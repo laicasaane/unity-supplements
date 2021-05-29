2cde01ce8eae68f9984a5398c2bab358b3b52a87
Laicasaane <laicasaane@outlook.com>
2021-05-29 21:58:09 +0700

    Values.Range methods now return array of T instead of IEnumerable<T> to reduce memory allocation and improve performance

---
+++ System.Collections.Generic/Values/Values.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Collections.Generic
+{
+    public readonly struct Values
+    {
+        public static Values<T> Range<T>(params T[] values)
+            => new Values<T>(values);
+    }
+
+    public static class ValuesExtensions
+    {
+        public static Values<T> Range<T>(this Values _, params T[] values)
+            => new Values<T>(values);
+    }
+}
---
+++ System.Collections.Generic/Values/Values{T}.cs
@@ -0,0 +1,88 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System.Collections.Generic
+{
+    public readonly struct Values<T> : IEnumerable<T>
+    {
+        private readonly T[] values;
+
+        public Values(T[] values)
+        {
+            this.values = values;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Enumerator GetEnumerator()
+            => new Enumerator(this.values);
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+            => GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        public static implicit operator Values<T>(T[] values)
+            => new Values<T>(values);
+
+        public struct Enumerator : IEnumerator<T>
+        {
+            private readonly T[] source;
+            private readonly long length;
+
+            private long current;
+            private bool first;
+
+            internal Enumerator(T[] array)
+            {
+                this.source = array ?? ReadArray1<T>.Empty.GetSource();
+                this.length = this.source.LongLength;
+                this.current = 0;
+                this.first = true;
+            }
+
+            public bool MoveNext()
+            {
+                if (this.length == 0)
+                    return false;
+
+                if (this.first)
+                {
+                    this.first = false;
+                    return true;
+                }
+
+                if (this.current < this.length - 1)
+                {
+                    this.current++;
+                    return true;
+                }
+
+                return false;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    if (this.current >= this.length)
+                        throw ThrowHelper.GetInvalidOperationException_InvalidOperation_EnumEnded();
+
+                    return this.source[this.current];
+                }
+            }
+
+            object IEnumerator.Current
+                => this.Current;
+
+            public void Reset()
+            {
+                this.current = 0;
+                this.first = true;
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
